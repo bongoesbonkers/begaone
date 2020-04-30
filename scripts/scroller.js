f527896c43bf7972fc8c21d2566ffb4db809a8eb
bongoesbonkers <56145096+bongoesbonkers@users.noreply.github.com>
2020-04-30 16:01:52 +0800

    Add files via upload

---
+++ scripts/scroller.js
@@ -0,0 +1,6 @@
+document.addEventListener( 'DOMContentLoaded', () => {
+    let featureCards = document.querySelectorAll('.feature-cards');
+    featureCards.forEach( collection => {
+        collection.scrollTop = collection.scrollHeight;
+    });
+});
