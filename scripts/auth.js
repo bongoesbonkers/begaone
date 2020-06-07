e843bc128d963f08e6d9b18efb284aa3511a682f
bongoesbonkers <56145096+bongoesbonkers@users.noreply.github.com>
2020-06-08 00:11:43 +0800

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -51,7 +51,7 @@ class Authenticator {
                         .then(document => {
                             if(document.exists) {
                                 const user = document.data();
-                                window.location.href = "../home.html";
+                                window.location.href = "home.html";
                             } else {
                                 //create new user document
                                 db.collection("users").doc(response.user.uid).set({
@@ -59,7 +59,7 @@ class Authenticator {
                                 })
                                 .then(()=>{
                                     //redirect to homepage after sign-in
-                                    window.location.href = "../home.html";
+                                    window.location.href = "home.html";
                                 })
                             }
                         })                        
@@ -81,7 +81,7 @@ class Authenticator {
                     console.log('user signed out');
 
                     //redirect user back to index
-                    window.location.href = "../index.html";
+                    window.location.href = "index.html";
                 })
                 .catch(err => console.log(err))
             })
@@ -96,7 +96,7 @@ class Authenticator {
                     const redirectToggler = document.querySelector('#redirectToggler');
                     redirectToggler.checked = true;
                     let timer = setTimeout(()=>{
-                        window.location.href = "../home.html";
+                        window.location.href = "home.html";
                     }, 1200)
                 }
                 console.log(`user is logged in`);
