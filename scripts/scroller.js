c9862fcf4e5fee6ddef50646149391799bb47227
bongoesbonkers <56145096+bongoesbonkers@users.noreply.github.com>
2020-06-09 10:59:12 +0800

    Add files via upload

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -1,17 +1,41 @@
 //JS for website innate scroll functionalities 
 
+//scroll in JS
+const sectionsContainer = document.querySelector('.sections__container');
+
+sectionsContainer.addEventListener('touchstart', e => {
+    if (e.target.tagName === 'UL' || e.target.tagName === 'A') {
+        const startX = e.touches[0].screenX;
+        sectionsContainer.addEventListener('touchmove', e => {
+            let finalX = e.touches[0].screenX;
+            const slideContent = sectionsContainer.querySelector('ul');
+            handleSlide(slideContent, startX, finalX);
+        });
+    }
+})
+
+function handleSlide(container, startPoint, endPoint) {
+    if(startPoint > endPoint) {
+        console.log('scroll to right');
+        container.scrollBy(0, -150);
+    } else {
+        console.log('scroll to left');
+        container.scrollBy(0, 150);
+    }
+}
+
+
 //scroll for home-page feature cards
-document.addEventListener( 'DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     let sliderContents = document.querySelectorAll('.slider__contents');
-    if(sliderContents){
-        sliderContents.forEach( content => {    
-            let timer = setTimeout(()=> {
+    if (sliderContents) {
+        sliderContents.forEach(content => {
+            let timer = setTimeout(() => {
                 content.scrollTop = content.scrollHeight;
                 clearTimeout(timer);
             }, 1300);
         });
     }
-
 });
 
-//scroll for business pages chat
+
