6697d6aafe32d52a0173198335cc686c494e8c32
bongoesbonkers <56145096+bongoesbonkers@users.noreply.github.com>
2020-06-09 14:38:47 +0800

    Add files via upload

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -3,27 +3,14 @@
 //scroll in JS
 const slideContents = document.querySelectorAll('.slider__contents');
 
-// sectionsContainer.addEventListener('touchstart', e => {
-//     const startX = e.touches[0].screenX;
-//     slideContents.forEach(function (slideContent) {
-//         sectionsContainer.addEventListener('touchmove', e => {
-//             if (e.target.tagName === 'UL' || e.target.tagName === 'A') {
-//                 let finalX = e.touches[0].screenX;
-//                 handleSlide(slideContent, startX, finalX);
-//             }
-//         })
-//     });
-// })
-
 if (slideContents) {
-    slideContents.forEach(function (content) {
+    slideContents.forEach(function(content) {
             content.addEventListener('touchstart', e => {
                 const startX = e.touches[0].clientX;
                 content.addEventListener('touchmove', e => {
                     let endX = e.touches[0].clientX;
                     let distance = endX- startX;
                     let rate = distance/100;
-                    console.log(rate);
                     if( rate > 1) {
                         content.scrollBy(0, distance*rate);
                     } else {
