eda98d8443274565a4fc2588301beadf16c79f30
bongoesbonkers <56145096+bongoesbonkers@users.noreply.github.com>
2020-06-09 14:41:25 +0800

    Add files via upload

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -1,35 +1,28 @@
 //JS for website innate scroll functionalities 
 
 //scroll in JS
-const slideContents = document.querySelectorAll('.slider__contents');
-
-if (slideContents) {
-    slideContents.forEach(function(content) {
+document.addEventListener('DOMContentLoaded', () => {
+    let slideContents = document.querySelectorAll('.slider__contents');
+    if (slideContents) {
+        slideContents.forEach(content => {
+            let timer = setTimeout(() => {
+                content.scrollTop = content.scrollHeight;
+                clearTimeout(timer);
+            }, 1300);
             content.addEventListener('touchstart', e => {
                 const startX = e.touches[0].clientX;
                 content.addEventListener('touchmove', e => {
                     let endX = e.touches[0].clientX;
-                    let distance = endX- startX;
-                    let rate = distance/100;
-                    if( rate > 1) {
-                        content.scrollBy(0, distance*rate);
+                    let distance = endX - startX;
+                    let rate = distance / 100;
+                    if (rate > 1) {
+                        content.scrollBy(0, distance * rate);
                     } else {
                         content.scrollBy(0, distance);
                     }
                 })
             })
         });
-    }
 
-    //scroll for home-page feature cards
-    document.addEventListener('DOMContentLoaded', () => {
-        let sliderContents = document.querySelectorAll('.slider__contents');
-        if (sliderContents) {
-            sliderContents.forEach(content => {
-                let timer = setTimeout(() => {
-                    content.scrollTop = content.scrollHeight;
-                    clearTimeout(timer);
-                }, 1300);
-            });
-        }
-    });
+    }
+});
