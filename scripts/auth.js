0b44a58071ac6413ec03da7748f3c69f2e84fa19
bongoesbonkers <56145096+bongoesbonkers@users.noreply.github.com>
2020-06-10 10:57:35 +0800

    Add files via upload

--- scripts/auth.js
+++ scripts/auth.js
@@ -7,120 +7,112 @@ const loginMessage = document.querySelector('.login__message-container');
 
 class Authenticator {
 
-    constructor () {
+    constructor() {
 
     }
 
-    register () {
-        if(registerForm) {
+    register() {
+        if (registerForm) {
             registerForm.addEventListener('submit', async e => {
                 e.preventDefault();
                 await auth.createUserWithEmailAndPassword(registerForm.registerEmail.value.trim(), registerForm.registerPassword.value.trim())
-                .then((response)=>{
-                    //TEST
-                    console.log(response);
+                    .then((response) => {
+                        //TEST
+                        console.log(response);
 
-                    //EXIT REGISTER FORM
-                    registerFormToggler.checked = false;
-                    //RESET REGISTER FORM
-                    registerForm.reset();
-                    //Sign user out
-                    auth.signOut();
-                    //Show message letting user know he is registered 
-                    //Prompt user to sign in
-                    indexUI.setCaption('Success! Please login with your e-mail and password.');
-                })
-                .catch((err)=>{
-                    //TEST
-                    console.log(err);
-                    indexUI.setCaption(err);
-                });
-            })
-        }
-    }
-
-    signIn () {
-        if(loginForm){
-            loginForm.addEventListener('submit', e => {
-                e.preventDefault();
-                auth.signInWithEmailAndPassword(loginForm.loginUsername.value, loginForm.loginPassword.value)
-                    .then((response)=>{
-                        // TEST
-                        // console.log(response);
-                        // Toggle Login Message
-                        loginMessage.style.zIndex = "200";
-                        loginMessage.classList.toggle('u-hidden');
-                        // Set user persistence
-                        const userPersist = loginForm.loginPersist.value;
-                        if(userPersist) {
-                            auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-                        } else {
-                            auth.setPersistence(firebase.auth.Auth.Persistence.NONE);
-                        }
-                        // CHECK IF USER DOCUMENT EXISTS
-                        db.collection("users").doc(response.user.uid).get()
-                        .then(async document => {
-                            if(document.exists) {
-                                const user = await document.data();
-                                window.location.href = "home.html";
-                            } else {
-                                //create new user document
-                                await db.collection("users").doc(response.user.uid).set({
-                                    email: response.user.email
-                                })
-                                .then(()=>{
-                                    //redirect to homepage after sign-in
-                                    window.location.href = "home.html";
-                                })
-                            }
-                        })                        
+                        //EXIT REGISTER FORM
+                        registerFormToggler.checked = false;
+                        //RESET REGISTER FORM
+                        registerForm.reset();
+                        //Sign user out
+                        auth.signOut();
+                        //Show message letting user know he is registered 
+                        //Prompt user to sign in
+                        indexUI.setCaption('Success! Please login with your e-mail and password.');
                     })
-                    //fail to sign in
                     .catch((err) => {
-                        indexUI.setCaption('That username/password is incorrect. Please try again.');
-                        return;
+                        //TEST
+                        console.log(err);
+                        indexUI.setCaption(err);
                     });
             })
         }
     }
 
-    signOut () {
-        if(signOutBtn) {
-            signOutBtn.addEventListener('click', e => {
-                e.preventDefault();
-                auth.signOut()
-                .then(()=>{
+    signInHandler() {
+        if (loginForm) {
+            loginForm.addEventListener('submit', e => {
+                    e.preventDefault();
+                    // SET USER PERSISTENCE
+                    const persistLogin = loginForm.loginPersist.checked;
+                    if (persistLogin) {
+                        auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+                            .then(() => {
+                                console.log('executing persistent login');
+                                this.signInWithEmailAndPassword();
+                            })
+                    } else {
+                        auth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+                            .then(() => {
+                                console.log('executing session login');
+                                this.signInWithEmailAndPassword();
+                            })
+                    }
+            })
+        }
+    }
+
+    signInWithEmailAndPassword() {
+        auth.signInWithEmailAndPassword(loginForm.loginUsername.value, loginForm.loginPassword.value)
+        .then((response) => {
+            // TEST
+            console.log(response);
+        })
+        //fail to sign in
+        .catch((err) => {
+            indexUI.setCaption('That username/password is incorrect. Please try again.');
+            return;
+        })
+    }
+
+    signOut() {
+    if (signOutBtn) {
+        signOutBtn.addEventListener('click', e => {
+            e.preventDefault();
+            auth.signOut()
+                .then(() => {
                     console.log('user signed out');
 
                     //redirect user back to index
                     window.location.href = "index.html";
                 })
                 .catch(err => console.log(err))
-            })
-        }
+        })
+    }
     }
 
     authChanges() {
-        auth.onAuthStateChanged(user => {
-            if(user){
-                // redirect user to home page if he is still logged in
-                if(
-                window.location.pathname == "/index.html" || 
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            // redirect user to home page if he is still logged in
+            if (
+                window.location.pathname == "/index.html" ||
                 window.location.pathname == "/begaone/index.html" ||
                 window.location.pathname == "/" ||
                 window.location.pathname == "/begaone/"
-                ) {
-                    authUI.persistenceMessage(user);
-                    let timer = setTimeout(()=>{
-                        window.location.href = "home.html";
-                    }, 1200)
-                }
-                console.log(`user is logged in`);
-                navUI.setCaption(user);
-            } else {
-                console.log(`no user is logged in`);
+            ) {
+                authUI.persistenceMessage(user);
+                let timer = setTimeout(() => {
+                    window.location.href = "home.html";
+                }, 1500)
             }
-        })
+            console.log(user);
+            console.log(`user is logged in`);
+            navUI.setCaption(user);
+        } else {
+            console.log(`no user is logged in`);
+        }
+    })
     }
 
 }
